d7002c015cbd8f03e19529d63c3fa6169dcd839e
Noman Anwar <hfznoman9@gmail.com>
2024-09-07 18:52:57 +0500

    change pics link name

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -7,10 +7,10 @@ import React from 'react'
 const Page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
+    style={{backgroundImage: "url(Mountains.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover overflow-y-auto utility'>
       {/* <div className='grid grid-rows-4 grid-flow-col gap-4'> */}
-      <div className="grid grid-cols-1 gap-0.5 lg:grid-cols-2 lg:gap-5 max-w-[90%] max-h-[90%]">
+      <div className="grid grid-cols-1 gap-0.5 lg:grid-cols-2 lg:gap-2 max-w-[90%] max-h-[90%]">
         {Projects.map((project, index) => (
           // <div className='grid grid-cols-12 gap-12 md:grid-cols-2 md:gap-5'>
             <ProjectCard
--- constants/index.ts
+++ constants/index.ts
@@ -72,7 +72,7 @@ export const SkillData = [
     {
       title: "NextJs Portfolio Work",
       text: "Its my portfolio on Nextjs by using typescript, tailwindcss,  framer motion, react, reacticons, shedcn etc ",
-      src: "/Webportfolio.png",
+      src: "/Webportfolio.PNG",
     },
     {
       title: " Add New Task",
