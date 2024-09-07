556338089c9f66f50e114f446cae9c772fbc61a7
Noman Anwar <hfznoman9@gmail.com>
2024-09-07 18:37:03 +0500

    change size in progect card folder

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
     style={{backgroundImage: "url(/mountains.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover overflow-y-auto utility'>
       {/* <div className='grid grid-rows-4 grid-flow-col gap-4'> */}
-      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-5 max-w-[90%] max-h-[90%]">
+      <div className="grid grid-cols-1 gap-0.5 lg:grid-cols-2 lg:gap-5 max-w-[90%] max-h-[90%]">
         {Projects.map((project, index) => (
           // <div className='grid grid-cols-12 gap-12 md:grid-cols-2 md:gap-5'>
             <ProjectCard
